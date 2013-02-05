b5f7796aea065c6291f1199d1aa616b0eabf13b4
Ndidi Alaneme <ndidia@cakesolutions.net>
2013-02-05 02:12:02 +0000

    Minor save image improvements

--- Resources/ui/common/SaveImageToCategoryWindow.js
+++ Resources/ui/common/SaveImageToCategoryWindow.js
@@ -1,11 +1,43 @@
 function SaveImageToCategoryWindow(image){
 	var self = Ti.UI.createWindow({
-		backgroundColor:'white'
+		backgroundColor:'white',
+		title:'Categories'
+	});
+	var db = require('db');
+	
+	//Right Nav Button
+	var addButton = Titanium.UI.createButton({title:'Add'});
+	addButton.addEventListener('click', function(e){
+		var dialog = Ti.UI.createAlertDialog({
+			title:'Choose category name',
+			style:Ti.UI.iPhone.AlertDialogStyle.PLAIN_TEXT_INPUT,
+			buttonNames:['Cancel', 'OK'],
+			cancel:0
+		});
+		dialog.addEventListener('click', function(e){
+			if(e.index === 1){
+				db.addCategory(e.text);
+				//TODO need to add image to database
+				//TODO go to detail view
+			}
+		});
+		dialog.show();
+	});
+	self.rightNavButton = addButton;
+	
+	//The Table
+	var categories = db.getAllCategories();
+	var data = [];
+	
+	for(var i=0,j=categories.length; i<j; i++){
+	  data[i] = Ti.UI.createTableViewRow({title:categories[i].name}) 
+	};
+	
+	var tableview = Titanium.UI.createTableView({
+		data:data
 	});
 	
-	var db = require('db');
-	db.getAllCategories();
-	
+	self.add(tableview);
 	return self;
 }
 
